2e419e0b933f87ed8ea630d6b93ceead2ced7aa8
huyongfa <yongfa.hu@auralic.com>
2015-08-26 09:34:06 +0800

    improve panic module, don't depend on uboot

--- drivers/auralic/auralic_panic.c
+++ drivers/auralic/auralic_panic.c
@@ -25,6 +25,8 @@
 #include <linux/kmsg_dump.h>
 #include "auralic_panic.h"
 
+#define  PANIC_LOG_FILE     "/home/root/kernel_panic.log"
+
 void __iomem *logbuff = NULL;
 
 static int aura_panic(struct notifier_block *n, unsigned long val, void *buff)
@@ -45,8 +47,10 @@ static struct notifier_block aura_notifier = {aura_panic, NULL, 1 };
 
 static int __init aura_panic_init(void)
 {
+    struct auralic_panic_head * panic = NULL;
+    
 	atomic_notifier_chain_register(&panic_notifier_list, &aura_notifier);
-    printk(KERN_DEBUG"enter aura panic\n");
+    printk(KERN_DEBUG"loading auralic panic module!\n");
     logbuff = ioremap(PHY_MEM_LOG_BUFF_ADDR, PHY_MEM_LOG_BUFF_SIZE);
     if(NULL == logbuff)
     {
@@ -55,16 +59,36 @@ static int __init aura_panic_init(void)
     }
     else
     {
+        panic = logbuff;
+        if(0 < panic->logsize && 0 == strncmp(panic->magic, PANIC_MAGIC, strlen(PANIC_MAGIC)))
+        {
+            mm_segment_t fs; 
+            unsigned long flags;
+            struct file *fp = NULL;
+
+            printk(KERN_DEBUG"auralic found panic log %d bytes!\n", panic->logsize);
+            fp =filp_open(PANIC_LOG_FILE, O_RDWR | O_CREAT, 0644);
+            if(!IS_ERR(fp))
+            {
+                fs = get_fs();
+                set_fs(KERNEL_DS);
+                fp->f_op->write(fp, panic->header, panic->logsize, &fp->f_pos);
+                filp_close(fp, NULL);
+                set_fs(fs);
+                printk(KERN_DEBUG"auralic save %s success!\n", PANIC_LOG_FILE);
+            }
+        }
+        
         memset((char *)logbuff, 0, PHY_MEM_LOG_BUFF_SIZE);
     }
-    printk(KERN_DEBUG"aura_panic ioremap success!\n");
+    printk(KERN_DEBUG"auralic panic ioremap success!\n");
 	return 0;
 }
 
 static void __exit aura_panic_exit(void)
 {
     iounmap(logbuff);
-    printk(KERN_DEBUG"leave auralic panic!\n");
+    printk(KERN_DEBUG"unloading auralic panic module!\n");
 }
 
 module_init(aura_panic_init);
