949ace5d197d90b2b8aefedca0e71ae572f7dda9
huyongfa <yongfa.hu@auralic.com>
2016-09-02 13:48:34 +0800

    add config for polaris, and update mini/aries/altair config

--- sound/usb/card.c
+++ sound/usb/card.c
@@ -523,6 +523,8 @@ static int usb_audio_probe(struct usb_interface *intf,
 			    (0x40 == USB_ID_PRODUCT(id))
 			    #elif defined(CONFIG_AURALIC_ALTAIR)
 			    (0x42 == USB_ID_PRODUCT(id))
+			    #elif defined(CONFIG_AURALIC_POLARIS)
+			    (0x44 == USB_ID_PRODUCT(id))
 			    #endif
 			    ) {
 				err = snd_usb_audio_create(intf, dev, 0, quirk,
